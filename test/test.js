633fa9cac4d66b5c2f23ec8114952bb8dda55afa
toff@myhost.fr <root@factory3-server-mgl7g7jfhri7.novalocal>
2016-02-23 14:29:59 +0000

    Modify test2

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello world 2' response body, got 'hello world 2 + CI + CD', done);
+    request(app).get('/').expect('hello world-CI-CD', done);
   });
 });
