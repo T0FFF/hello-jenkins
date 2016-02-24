0fbc7dc79851e3faf70d5d1653343396035dc401
toff@myhost.fr <root@factory3-server-mgl7g7jfhri7.novalocal>
2016-02-24 10:25:33 +0000

    Change html content

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello world-CI-CD2');
+  res.send('hello world-CI-CD');
 });
  
 app.listen(process.env.PORT || 5000);
