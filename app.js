b8c92936ebdb074e23acf55b8368049afbeb5509
toff@myhost.fr <root@factory3-server-mgl7g7jfhri7.novalocal>
2016-03-29 11:49:02 +0200

    Add server hostname

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello world-CI-CD');
+//  res.send('Hello world-CI-CD');
+   res.send('Hello world-CI-CD: <b><i>' + require('os').hostname() + '</i></b>');
 });
  
 app.listen(process.env.PORT || 5000);
